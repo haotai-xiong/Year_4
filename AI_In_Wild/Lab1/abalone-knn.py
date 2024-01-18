94a965e527227446516e4a5f160e7408f4453758
haotai-xiong <c00265675@itcarlow.ie>
2024-01-18 15:19:51 +0000

    ai in the wild - lab1 not finished

---
+++ AI_In_Wild/Lab1/abalone-knn.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+url = (
+    "https://archive.ics.uci.edu/ml/machine-learning-databases"
+    "/abalone/abalone.data"
+)
+abalone = pd.read_csv(url, header=None)
+
+abalone.head()
