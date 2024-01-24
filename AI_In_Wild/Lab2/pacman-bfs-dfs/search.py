cfaf61852c02de65d1a4ecf9261ad484d54ccc9a
gameuser <harritiexiong@outlook.com>
2024-01-24 17:01:18 +0000

    lab 2 init #3

--- AI_In_Wild/Lab2/pacman-bfs-dfs/search.py
+++ AI_In_Wild/Lab2/pacman-bfs-dfs/search.py
@@ -114,6 +114,7 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+
     util.raiseNotDefined()
     
 def uniformCostSearch(problem):
